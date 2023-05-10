30430a52bdf64338827359eb8ea57993e5f1cdcc
lwertis <wertisml@appstate.edu>
2023-05-10 14:54:37 -0400

    created the renv and polished the Model Performance code

--- Code/06.Model_Performance.R
+++ Code/06.Model_Performance.R
@@ -8,24 +8,13 @@ library(gam)
 library(randomForest)
 library(xgboost)
 
-library(psych)
-library(caret)
-
-library(RSNNS)
-library(e1071)
-library(ranger)
-
-library(ggpubr)
-
-
-
 setwd("~/Temperature_Socioeconomic_Health_Response/Files")
 
 #==============================================================================#
 # Input data
 #==============================================================================#
 
-Data <- fread("North_Carolina_sheps_temp.csv")
+Data <- fread("North_Carolina_sheps_temp_over_25.csv")
 
 # Remove the NA rows
 Data <- Data[complete.cases(Data),]
@@ -58,21 +47,15 @@ MAE <- function(Obs, Pred){
   return(MAE)
 }
 
-MAPE <- function(Obs, Pred){
-  Dif <- Pred - Obs
-  MAPE <- mean(abs(Dif/Obs)*100) %>% round(., 2)
-  return(MAPE)
-}
-
 #==============================================================================#
 # Performance All Mental_Health in Train and Test
 #==============================================================================#
 
-setwd("~/Temperature_Socioeconomic_Health_Response/Model_rds")
-Models <- c("GLM.rds",
-            "GAM.rds",
-            "RF.rds",
-            "XGBoost.rds")
+setwd("~/Temperature_Socioeconomic_Health_Response/Model_rds/Over_25")
+Models <- c("GLM_over_25.rds",
+            "GAM_over_25.rds",
+            "RF_over_25.rds",
+            "XGBoost_over_25.rds")
 
 # get model list
 Model_list <- Models
@@ -90,45 +73,18 @@ for(iii in Model_list){
   Train$Pred <- if(iii == "glm_uni.rds"){Train$Pred <- predict(Model, Train, type = "response")}else{Train$Pred <- predict(Model, Train)}
   Train$Pred <- ifelse(Train$Pred < 0, 0, Train$Pred)
   
-  # get performance
+  # get performance on train
   RMSE_train <- RMSE(Obs = Train$Mental_Health, Pred = Train$Pred)
-  
   MAE_train <- MAE(Obs = Train$Mental_Health, Pred = Train$Pred)
-  
-  MAPE_train <- MAPE(Obs = Train$Mental_Health, Pred = Train$Pred)
-  
-  Cor_train <- paste(
-    cor.test(Train$Mental_Health, Train$Pred)$estimate %>% round(., 2),
-    " (",
-    cor.test(Train$Mental_Health, Train$Pred)$conf.int[1] %>% round(., 2),
-    " to ",
-    cor.test(Train$Mental_Health, Train$Pred)$conf.int[2] %>% round(., 2),
-    ")",
-    sep = ""
-  )
   
   # get predicted values
   Test$Pred <- if(iii == "glm_uni.rds"){Test$Pred <- predict(Model, Test, type = "response")}else{Test$Pred <- predict(Model, Test)}
   Test$Pred <- ifelse(Test$Pred < 0, 0, Test$Pred)
   
-  # get performance
+  # get performance on test
   RMSE_test <- RMSE(Obs = Test$Mental_Health, Pred = Test$Pred)
-  
-  nRMSE_test <- round(RMSE(Obs = Test$Mental_Health, Pred = Test$Pred) / mean(Test$Mental_Health), 3)
-  
   MAE_test <- MAE(Obs = Test$Mental_Health, Pred = Test$Pred)
   
-  nMAE_test <- round(MAE(Obs = Test$Mental_Health, Pred = Test$Pred) / mean(Test$Mental_Health), 3)
-  
-  Cor_test <- paste(
-    cor.test(Test$Mental_Health, Test$Pred)$estimate %>% round(., 2),
-    " (",
-    cor.test(Test$Mental_Health, Test$Pred)$conf.int[1] %>% round(., 2),
-    " to ",
-    cor.test(Test$Mental_Health, Test$Pred)$conf.int[2] %>% round(., 2),
-    ")",
-    sep = ""
-  )
   
   # Summarize
   Performance <- c(iii,
@@ -147,36 +103,7 @@ Hoge
 fwrite(Hoge, "RMSE.csv")
 
 #==============================================================================#
-# Variable Importance
-#==============================================================================#
-
-# get model list
-Model_list <- Models
-
-# loop
-for(iii in Model_list){
-  
-  # load models
-  Model <- readRDS(iii)
-  
-  varimp_data <- data.frame(feature = row.names(varImp(Model))[1:5],
-                            importance = varImp(Model)[1:5, 1])
-  
-  varimp_data$feature <- factor(varimp_data$feature, 
-                                levels = varimp_data$feature[order(varimp_data$importance, 
-                                                                   decreasing = FALSE)])
-  
-  ggplot(data = varimp_data, aes(x = feature, y = importance)) +
-    labs(x = "Features", y = "Variable Importance") + 
-    geom_point(stat="identity") +
-    coord_flip() + 
-    theme_bw() + theme(legend.position = "none")
-  ggsave(paste("", iii, "_variable_importance.png", sep = ""), width = 4.2, height = 3.2) #*action
-  
-}
-
-#==============================================================================#
-# Figure time series by 24h
+# Daily Prediction vs Actual
 #==============================================================================#
 
 # get model list
@@ -333,121 +260,4 @@ for(iii in Model_list){
 Hoge <- data.table(Hoge)
 Hoge
 
-#==============================================================================#
-# Bonus Stuff
-#==============================================================================#
 
-# formula
-Form_temp <- formula( 	
-  Mental_Health ~ 
-    offset(log_Total_Pop_per1000)  
-  #+ Median_Age  
-  #+ Pop_5_24_per1000 
-  + Male_to_Female_Ratio 
-  + RUCA1 
-  + Region
-  + Income 
-  + Race 
-  
-  + Day 
-  + month 
-  + NDVI 
-  
-  #+ TAVG  
-  + TMIN 
-  + TMAX 
-  #+ TAVGLag1 
-  #+ TMINLag1 
-  #+ TMAXLag1 
-  #+ TAVG_24hr_diff 
-  + TMIN_24hr_diff 
-  + TMAX_24hr_diff 
-  #+ Daily_Difference 
-  + EHF 
-  + RH 
-  #+ Heat_Index 
-  #+ Discomfort_Index
-)
-
-#==============================================================================#
-# Variance Inflation Factors
-#==============================================================================#
-
-Model_vif <- glm(family = "poisson",
-                 Form_temp,
-                 data = Train)
-
-summary(Model_vif)
-vif(Model_vif)
-
-#==============================================================================#
-# Correlation Matrix
-#==============================================================================#
-
-my_data <- Data[, c("Male_to_Female_Ratio", "RUCA1", "Region", "Income", "Race",
-                    "Day", "month", "NDVI", "TMIN", "TMAX", "TMAX_24hr_diff", 
-                    "EHF", "RH")]
-res <- cor(my_data)
-corrplot(res, type = "upper", order = "hclust", 
-         tl.col = "black", tl.srt = 45)
-
-#==============================================================================#
-# Plotting Cool Stuff
-#==============================================================================#
-
-Cases_data <- Data %>%
-  mutate(Year = as.factor(year(Date))) %>%
-  select(Mental_Health, Year)
-
-# Box plot of cases per year
-Tree_intensity_long <- melt(Cases_data, id = "Year") 
-
-ggplot(Tree_intensity_long, aes(x = variable, y = value, fill = Year)) +  # ggplot function
-  geom_boxplot(outlier.alpha = 0.5)+
-  coord_flip() +
-  theme_bw() +
-  labs(fill = NULL, x = NULL,
-       y = "Case breakdown by year",
-       title = "Frequency of any mental health ICD code") +
-  theme(plot.title = element_text(hjust = 0.5))
-
-# Region
-Cases_data <- Data %>%
-  mutate(Year = as.factor(year(Date))) %>%
-  select(Mental_Health, Region) 
-
-Cases_data$Region <- as.factor(Cases_data$Region)
-
-# Box plot of cases per year
-Tree_intensity_long <- melt(Cases_data, id = "Region") 
-
-ggplot(Tree_intensity_long, aes(x = variable, y = value, fill = Region)) +  # ggplot function
-  geom_boxplot(outlier.alpha = 0.5)+
-  coord_flip() +
-  theme_bw() +
-  labs(fill = NULL, x = NULL,
-       y = "Case breakdown by Region",
-       title = "Frequency of any mental health ICD code") +
-  theme(plot.title = element_text(hjust = 0.5))
-
-#==============================================================================#
-
-Datar <- Data %>%
-  group_by(Date) %>%
-  summarize(Mental_Health = sum(Mental_Health)) %>%
-  mutate(Day = 1:length(Mental_Health),
-         Year = as.character(year(Date)),
-         month = as.character(month(Date))) %>% 
-  select(Date, Mental_Health, Day, Year, month)
-
-# Mental Health
-ggplot(data = Datar, aes(x = Mental_Health, color = Year)) +
-  geom_line(aes(y = Mental_Health, x = Day), size = 0.4) +
-  theme_classic() 
-
-#==============================================================================#
-
-Temp <- Data %>%
-  group_by(Region, RUCA1) %>%
-  summarize(mean_TAVG = mean(TAVG),
-            meadian_TAVG = median(TAVG))
